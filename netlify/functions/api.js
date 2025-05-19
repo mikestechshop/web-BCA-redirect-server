4f84a976362496598f9f84a11e8f32a2156b084e
jlunardini <john-carlos.lunardini@mikestechshop.com>
2025-05-19 15:41:42 -0400

    change entry point

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,8 +3,15 @@ import serverless from "serverless-http";
 
 const api = express();
 
-const router = Router();
+// Add logging middleware
+api.use((req, res, next) => {
+  console.log("Function hit:", req.method, req.path);
+  next();
+});
+
+// Middleware to redirect all traffic
 api.use((req, res) => {
+  console.log("Redirecting to goforpink");
   res.redirect(301, "https://breastcanceralliance.org/goforpink");
 });
 
